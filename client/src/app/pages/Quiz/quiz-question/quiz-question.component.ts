b47aee7c3e220a6cae3f9637dccd306f8ef2c65f
Fkoc <vinhtuan18@gmail.com>
2025-04-26 02:49:31 +0700

    Fetching quiz data from database

--- client/src/app/pages/Quiz/quiz-question/quiz-question.component.ts
+++ client/src/app/pages/Quiz/quiz-question/quiz-question.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { Location, NgClass, CommonModule } from '@angular/common'; // Import CommonModule
-import { QuizService } from '../../../services/quiz/quiz.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 // Define Question interface
-interface Question {
-  id: number;
-  level: string;
-  question: string;
-  options: string[];
-  answer: string;
-  url?: string; // Optional url field
+interface RawQuestion {
+	id: string;
+	level: 'easy' | 'medium' | 'hard';
+	question: string;
+	answerA: string;
+	answerB: string;
+	answerC: string;
+	answerD: string;
+	correctAnswer: 'A' | 'B' | 'C' | 'D';
+	image?: string;
+	score: number;
+}
+
+interface QuizQuestion extends RawQuestion {
+	options: string[];
+	answer: string;
 }
 
 @Component({
@@ -20,119 +31,143 @@ interface Question {
   templateUrl: './quiz-question.component.html',
   styleUrl: './quiz-question.component.scss'
 })
+
+
 export class QuizQuestionComponent {
-  constructor(
-    private location: Location,
-    private quizService: QuizService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) {}
+	questions: QuizQuestion[] = [];
+	currentQuestion: QuizQuestion | null = null;
+	currentIndex: number = 0;
+	level: string = '';
+	score: number = 0;
+	selectedOption: string | null = null;
+	answered: boolean = false;
+	
+    constructor(
+        private location: Location,
+        private router: Router,
+        private route: ActivatedRoute,
+        private http: HttpClient,
+    ) {}
 
-  shuffledOptions: string[] = [];
-  questions: any[] = [];
-  currentQuestion: any = null;
-  currentIndex: number = 0;
-  level= '';
-  score = 0;
-
-  selectedOption: string | null = null;
-  answered = false;
-
-  ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      this.level = params['level'] || '';
-      let filteredQuestions;
-
-      if (this.level === 'mixed') {
-        filteredQuestions = this.quizService.questions;
-      } else {
-        filteredQuestions = this.quizService.questions.filter(q => q.level === this.level);
-      }
-
-      if (filteredQuestions.length === 0) {
-        console.log('No questions found for level:', this.level);
-      }
-
-      const shuffledQuestion = [...filteredQuestions].sort(() => Math.random()- 0.5);
-      this.questions = shuffledQuestion.slice(0, 5) ;
-
-      this.currentIndex = 0;
-      this.currentQuestion = this.questions[this.currentIndex];
-      this.score = 0;
-      this.selectedOption = null;
-      this.answered = false;
-      this.setShuffledOptions();
-    })
-  }
-
-  setShuffledOptions(): void {
-    if(!this.currentQuestion){
-      this.shuffledOptions = [];
-      return;
+    ngOnInit(): void {
+        this.route.queryParams.subscribe(params => {
+            this.level = params['level'] || 'easy';
+            this.fetchQuiz(this.level);
+        });
     }
-    const options = [
-      this.currentQuestion.option1,
-      this.currentQuestion.option2,
-      this.currentQuestion.option3,
-      this.currentQuestion.option4,
-    ];
-    this.shuffledOptions = this.shuffleArray(options);
-  }
-  
 
-  private shuffleArray(array: string[]):string[]{
+    fetchQuiz(level: string): void{
+        let apiUrl = '';
+
+        switch(level){
+            case 'easy':
+                apiUrl = 'http://localhost:8000/api/quiz/getEasyQuiz';
+                break;
+            case 'medium':
+                apiUrl = 'http://localhost:8000/api/quiz/getMediumQuiz';
+                break;
+            case 'difficult':
+                apiUrl = 'http://localhost:8000/api/quiz/getHardQuiz';
+                break;
+            case 'mixed':
+                apiUrl = 'http://localhost:8000/api/quiz/getMixQuiz';
+                break;
+            default:
+                alert('Invalid quiz level!');
+                this.router.navigate(['/quiz']);
+                return;
+        }
+
+        this.http
+            .get<any>(apiUrl)
+            .pipe(
+                catchError((err) => {
+                    alert('Error fetching quiz!');
+                    this.router.navigate(['/quiz']);
+                    return of(null);
+                })
+            )
+            .subscribe((res) => {
+                console.log('Quiz data:', res);
+                if(!res || !res.success || !res.data){
+                    alert('No quiz data received!');
+                    this.router.navigate(['/quiz']);
+                    return;
+                }
+
+                this.questions = res.data.map((q: RawQuestion) => {
+                    const optionMap = {
+                        A: q.answerA,
+                        B: q.answerB,
+                        C: q.answerC,
+                        D: q.answerD,
+                    };
+
+                    const options = this.shuffleArray([
+                        q.answerA,
+                        q.answerB,
+                        q.answerC,
+                        q.answerD,
+                    ])
+                    return {
+                        ...q,
+                        options,
+                        answer: optionMap[q.correctAnswer],
+                    };
+                });
+
+                this.currentIndex = 0;
+                this.score = 0;
+                this.setCurrentQuestion();
+            });
+    }
+
+    setCurrentQuestion(): void{
+        this.currentQuestion = this.questions[this.currentIndex];
+        this.selectedOption = null;
+        this.answered = false;
+    }
+
+    selectAnswer(option: string): void{
+        if(this.answered || !this.currentQuestion) return;
+
+        this.selectedOption = option;
+        this.answered = true;
+
+        if(option === this.currentQuestion.answer){
+            this.score++;
+        }
+
+        setTimeout(() => this.nextQuestion(), 1300);
+    }
+
+    nextQuestion(): void{
+        this.currentIndex++;
+
+        if(this.currentIndex < this. questions.length){
+            this.setCurrentQuestion();
+        }else{
+            alert(`Quiz finished! Your score: ${this.score}/${this.questions.length}`);
+            this.router.navigate(['/quiz']);
+        }
+    }
+
+    private shuffleArray(array: string[]):string[]{
     return array
-      .map(value => ({ value, sort: Math.random()}))
-      .sort((a, b) => a.sort - b.sort)
-      .map(({ value }) => value);
-  }
-
-  // getCurrentOptions(): string[] {
-  //   if(!this.currentQuestion) return [];
-  //   const options = [
-  //     this.currentQuestion.option1,
-  //     this.currentQuestion.option2,
-  //     this.currentQuestion.option3,
-  //     this.currentQuestion.option4
-  //   ]
-  //   return this.shuffleArray(options);
-  // }
-
-  selectAnswer(option: string): void {
-    if (this.answered) return;
-    this.selectedOption = option;
-    this.answered = true;
-
-    if (option === this.currentQuestion?.answer) {
-      this.score++;
+        .map(value => ({ value, sort: Math.random()}))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value);
     }
-    setTimeout(() => this.nextQuestion(), 1300);
-  }
 
-  nextQuestion(): void {
-    this.currentIndex++;
-    this.selectedOption = null;
-    this.answered = false;
-
-    if (this.currentIndex < this.questions.length) {
-      this.currentQuestion = this.questions[this.currentIndex];
-      this.setShuffledOptions();
-    } else {
-      this.currentQuestion = null;
-      alert(`Quiz finished: Your score: ${this.score}/${this.questions.length}`);
-      this.router.navigate(['/quiz']);
+    goBack(): void {
+        this.location.back();
     }
-  }
 
-  goBack(): void {
-    this.location.back();
-  }
+    getOptionLabel(index: number): string {
+        return String.fromCharCode(65 + index);
+    }
 
-  getOptionLabel(index: number): string {
-    return String.fromCharCode(65 + index);
-  }
-
-  trackOption(index: number, option: string): string {
-    return option;
-  }
+    trackOption(index: number, option: string): string {
+        return option;
+    }
 }
