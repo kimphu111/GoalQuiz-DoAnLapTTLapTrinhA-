93c1f4bfa816fb7864f37ebfeb47370aa5f1700f
Fkoc <vinhtuan18@gmail.com>
2025-05-17 01:18:36 +0700

    done question management

--- client/src/app/pages/admin-only/quiz-edit-admin/quiz-edit-admin.component.ts
+++ client/src/app/pages/admin-only/quiz-edit-admin/quiz-edit-admin.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {DatePipe, NgForOf, NgIf} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-quiz-edit-admin',
@@ -17,6 +18,65 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
   templateUrl: './quiz-edit-admin.component.html',
   styleUrl: './quiz-edit-admin.component.scss'
 })
-export class QuizEditAdminComponent {
+export class QuizEditAdminComponent implements OnInit {
+  quizzes: any[] = [];
+  showPopup = false;
+  selectedIndex: number | null = null;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.getAllQuiz();
+  }
+
+  getAllQuiz(){
+    const token = localStorage.getItem('accessToken');
+    this.http.get<any>('http://localhost:8000/api/quiz/getAllQuiz', {
+       headers: {
+        Authorization: `Bearer ${token}`
+    }
+    })
+    .subscribe({
+      next: res => {
+        // this.quizzes = res.quizzes.map((q: any) => ({
+        //   ...q,
+        //   participants: Array.isArray(q.players) && q.players.length> 0 ? q.players.length: 0
+        // }));
+        this.quizzes = res.quizzes;
+    },
+    error: err => {
+      console.log('Api error', err);
+    }
+  });
+  }
+
+  deleteQuiz(id:string, index: number){
+    const token = localStorage.getItem('accessToken');
+    console.log('Delete quiz', id, index);
+    this.http.delete<any>(`http://localhost:8000/api/quiz/deleteQuiz/${id}`,{
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).subscribe({
+      next: res => {
+        console.log('Delete succes', res);
+        this.quizzes.splice(index, 1);
+        this.closePopup();
+      },
+      error: err =>{
+        console.log('Delete ERROR', err);
+      }
+    });
+  }
+
+  openPopup(index:number) {
+    this.showPopup = true;
+    this.selectedIndex = index;
+  }
+
+  closePopup() {
+    this.showPopup = false;
+    this.selectedIndex = null;
+  }
 
 }
